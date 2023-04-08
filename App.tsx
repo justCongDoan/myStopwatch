6bf7802a3a058c79cc5732e113dfa244ca9c9a0e
justCongDoan <congdoan2002tb2@gmail.com>
2023-04-08 16:07:28 +0700

    2nd update - removed comment

--- App.tsx
+++ App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React, { Component } from 'react';
 import {Text, View, ScrollView, StyleSheet, TouchableHighlight} from 'react-native';
 import formatTime from 'minutes-seconds-milliseconds';
